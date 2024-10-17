ceecf3011a5f80a6b82c6cfc8e5ed5062db63530
Ashank Kumar Singh <Ashank.KumarSingh@philips.com>
2024-10-17 11:47:09 +0530

    Optimizing while extracting pure functions.

--- monitor.cpp
+++ monitor.cpp
@@ -1,37 +1,42 @@
 #include "./monitor.h"
+#include "./monitor.h"
 #include <assert.h>
 #include <thread>
 #include <chrono>
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
+bool isTemperatureCritical(float temperature) {
+  return temperature > 102 || temperature < 95;
+}
+
+bool isPulseRateOutOfRange(float pulseRate) {
+  return pulseRate < 60 || pulseRate > 100;
+}
+
+bool isSpo2OutOfRange(float spo2) {
+  return spo2 < 90;
+}
+
+void displayWarningMessage(const std::string& message) {
+  cout << message << "\n";
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
+}
+
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  if (isTemperatureCritical(temperature)) {
+    displayWarningMessage("Temperature is critical!");
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  } else if (isPulseRateOutOfRange(pulseRate)) {
+    displayWarningMessage("Pulse Rate is out of range!");
     return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  } else if (isSpo2OutOfRange(spo2)) {
+    displayWarningMessage("Oxygen Saturation out of range!");
     return 0;
   }
   return 1;
